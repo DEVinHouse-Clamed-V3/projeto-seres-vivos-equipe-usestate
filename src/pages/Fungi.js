ed4ad381cf47e08ced878eeb8c5db0135238cde4
CarlaCristina-s <cacris.bio@gmail.com>
2024-09-28 12:29:00 -0300

    Adição de filtro

--- src/pages/Fungi.js
+++ src/pages/Fungi.js
@@ -8,46 +8,121 @@ import {
   StyleSheet,
   SafeAreaView,
   Alert,
+  Modal,
+  TouchableOpacity,
+  ImageBackground,
+  TextInput,
 } from "react-native";
 
 const Fungos = () => {
+  const [modalVisible, setModalVisible] = useState(false);
   const [fungos, setFungos] = useState([]);
+  const [search, setSearch] = useState("");
+  const [filteredFungos, setFilteredFungos] = useState(fungos);
 
   useEffect(() => {
     axios;
     axios
       .get("http://192.168.1.70:3000/fungos")
       .then((response) => {
-      setFungos(response.data);
+        setFungos(response.data);
+        setFilteredFungos(response.data);
       })
       .catch(() => {
         Alert.alert("Não foi possível obter a lista do Reino Fungi");
       });
   }, []);
 
+  const handleSearch = (text) => {
+    setSearch(text);
+
+    if (text === "") {
+      setFilteredFungos(fungos);
+    } else {
+      const filtered = fungos.filter((item) =>
+        item.name.toLowerCase().includes(text.toLowerCase())
+      );
+      setFilteredFungos(filtered);
+    }
+  };
+
+  function Spacer(props) {
+    return <View style={{ height: props.size, width: props.size }} />;
+  }
+
   return (
     <SafeAreaView style={styles.container}>
+      <TouchableOpacity
+        style={styles.buttonInfo}
+        title="Nova Tarefa"
+        onPress={() => setModalVisible(true)}
+      >
+        <Text style={styles.buttonInfoText}>
+          Quer conhecer as principais caracteríscas dos fungos? Clique Aqui!
+        </Text>
+      </TouchableOpacity>
+
+      <TextInput
+        style={styles.inputCards}
+        placeholder="Buscar fungos"
+        value={search}
+        placeholderTextColor="#ccc"
+        maxLength={25}
+        autoCorrect={true}
+        onChangeText={handleSearch}
+      />
+
+      <Modal
+        animationType="slide"
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.containerModal}>
+          <ImageBackground
+            source={require("../../assets/fun.jpg")}
+            resizeMode="cover"
+            style={{ width: "100%", height: "100%", flex: 1 }}
+          >
+            <View style={styles.intro}>
+              <Text style={styles.introText}>
+                Os fungos são seres heterótrofos, eucariontes, unicelulares ou
+                pluricelulares. Se reproduzem de forma sexuada ou assexuada.
+              </Text>
+              <Spacer size={10} />
+              <Text style={styles.introText}>
+                São encontrados em quase todos os lugares, como no ar, no solo,
+                nas plantas e até nos alimentos.
+              </Text>
+              <Spacer size={10} />
+              <Text style={styles.introText}>
+                Hoje são classificados em cinco grupos principais: quitrídeos,
+                zigomicetos, glomeromicetos, ascomicetos e basidiomicetos.
+              </Text>
+              <Spacer size={10} />
+              <Text style={styles.introText}>
+                Junto com as bactérias, atuam na decomposição da matéria
+                orgânica. Além disso, são bastante utilizados na culinária e na
+                indústria farmacêutica.
+              </Text>
+              <Spacer size={10} />
+              <Text style={styles.introText}>
+                É importante lembrar que os fungos podem causar doenças, tais
+                como: Onicomicose, Candidíase, Rinossinusite, Meningite fúngica
+                e Pneumocistose.
+              </Text>
+            </View>
+          </ImageBackground>
+        </View>
+      </Modal>
+
       <FlatList
-        data={fungos}
+        data={filteredFungos}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
           <View style={styles.card}>
             <Image source={{ uri: item.image }} style={styles.image} />
             <Text style={styles.name}>{item.name}</Text>
             <Text style={styles.description}>{item.description}</Text>
-            <Text style={styles.othersInfos}>Nutrição: {item.nutrition}</Text>
-            <Text style={styles.othersInfos}>
-              Tipo Celular: {item.cellType}
-            </Text>
-            <Text style={styles.othersInfos}>
-              Organização Celular: {item.cellOrganization}
-            </Text>
-            <Text style={styles.othersInfos}>
-              Reprodução: {item.reproduction}
-            </Text>
-            <Text style={styles.othersInfos}>
-              Respiração: {item.respiration}
-            </Text>
           </View>
         )}
       />
@@ -58,6 +133,66 @@ const Fungos = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#fff",
+  },
+  textInitial: {
+    fontSize: 20,
+    color: "#9f9f9f",
+    textAlign: "center",
+    marginVertical: 10,
+  },
+  buttonInfo: {
+    width: "90%",
+    backgroundColor: "#fff",
+    padding: 10,
+    marginVertical: 10,
+    alignSelf: "center",
+    borderRadius: 5,
+    borderColor: "#006414",
+    shadowColor: "#006414",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 1,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  buttonInfoText: {
+    color: "#9f9f9f",
+    textAlign: "center",
+    fontSize: 16,
+  },
+  inputCards: {
+    width: "90%",
+    backgroundColor: "#fff",
+    padding: 10,
+    marginVertical: 10,
+    alignSelf: "center",
+    borderRadius: 5,
+    borderColor: "#006414",
+    shadowColor: "#006414",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 1,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  containerModal: {
+    height: "100%",
+  },
+  intro: {
+    marginTop: 80,
+    marginHorizontal: 10,
+  },
+  introText: {
+    fontSize: 22,
+    color: "#fff",
+    textAlign: "center",
+    lineHeight: 26,
+    alignItems: "center",
   },
   card: {
     padding: 15,
@@ -77,8 +212,8 @@ const styles = StyleSheet.create({
     elevation: 5,
   },
   image: {
-    width: 250,
-    height: 200,
+    width: 300,
+    height: 180,
     borderRadius: 10,
     marginVertical: 10,
     borderWidth: 1,
@@ -95,13 +230,8 @@ const styles = StyleSheet.create({
   description: {
     fontSize: 16,
     marginBottom: 10,
-    color: "#006414",
-    alignSelf: "center",
-  },
-  othersInfos: {
-    fontSize: 16,
-    lineHeight: 25,
     color: "#9f9f9f",
+    alignSelf: "center",
   },
 });
 
